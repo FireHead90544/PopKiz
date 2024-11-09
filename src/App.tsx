e45f44965dcc6e9093ab4e7d03fcf47285b9c26e
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-11-09 20:05:02 +0530

    setup react app

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import BirthdayCountdown from "./components/BirthdayCountdown";
+import WelcomeScreen from "./components/WelcomeScreen";
+import PuzzleQuest from "./components/PuzzleQuest";
+import MemoryLane from "./components/MemoryLane";
+import MemoryMatch from "./components/MemoryMatch";
+import TriviaFun from "./components/TriviaFun";
+import VirtualGiftBox from "./components/VirtualGiftBox";
+import BirthdayMessage from "./components/BirthdayMessage";
+
+const App: React.FC = () => {
+  const stages: Record<number, React.ComponentType<{ onNext: () => void }>> = {
+    1: BirthdayCountdown,
+    2: WelcomeScreen,
+    3: PuzzleQuest,
+    4: MemoryLane,
+    5: MemoryMatch,
+    6: TriviaFun,
+    7: VirtualGiftBox,
+    8: BirthdayMessage
+  }
+  const [stage, setStage] = useState(1);
+  const CurrentStage = stages[stage];
+
+  const goToNextStage = () => setStage(stage + 1);
+
+  return (
+    <div className="App">
+      <CurrentStage onNext={goToNextStage} />
+      {/* {stage < 8 && (
+        <button
+          onClick={goToNextStage}
+          className="fixed bottom-10 right-10 bg-purple-600 text-white py-2 px-4 rounded-lg hover:bg-purple-700 transition"
+        >
+          Next
+        </button>
+      )} */}
+    </div>
+  );
+};
+
+export default App;
